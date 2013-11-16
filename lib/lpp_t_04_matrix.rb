60daa84f620f0d79e7efbb8e125d11c2645e60ed
alu0100596113 <alu0100596113@ull.edu.es>
2013-11-16 16:46:57 +0000

    Se incluye espectativa para almacenar el numero de filas y columnas de la Clase Matriz.

--- lib/lpp_t_04_matrix.rb
+++ lib/lpp_t_04_matrix.rb
@@ -1,6 +1,7 @@
 require "lpp_t_04_matrix/version"
 
 module LppT04Matrix
-  class Matriz 
+  class Matriz
+
   end
 end
--- spec/lpp_t_04_matrix_spec.rb
+++ spec/lpp_t_04_matrix_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 include  LppT04Matrix
+
 describe LppT04Matrix do
+	
 	describe Matriz do
+		
+		describe "Se comprueba el numero de:" do
+			it "filas de la matriz" do
+				@m1.filas.should eq(2)  
+			end
+			it "columnas de la matriz" do
+				@m1.columnas.should eq(2)  
+			end
+		end
 	end
 end
