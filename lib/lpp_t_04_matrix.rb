799b52126c8099492fa9f18e76d7efa9812fb3ee
alu0100596113 <alu0100596113@ull.edu.es>
2013-11-16 17:02:56 +0000

    Se cumple espectativa para matriz Densa y Matriz Dispersa.

--- lib/lpp_t_04_matrix.rb
+++ lib/lpp_t_04_matrix.rb
@@ -1,11 +1,16 @@
 require "lpp_t_04_matrix/version"
 
 module LppT04Matrix
-  class Matriz
-  	attr_accessor :filas, :columnas
-  	def initialize (filas, columnas)
-  		@filas = filas
-  		@columnas = columnas
-  	end
-  end
+	class Matriz
+	  	attr_accessor :filas, :columnas
+	  	def initialize (filas, columnas)
+	  		@filas = filas
+	  		@columnas = columnas
+	  	end
+	end
+
+	class MatrizDensa < Matriz
+	end
+	class MatrizDispersa < Matriz
+	end
 end
