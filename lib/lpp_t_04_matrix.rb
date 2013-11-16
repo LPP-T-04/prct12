da54a2d5cac5a217d34602774f6f2be4c149a91d
alu0100596113 <alu0100596113@ull.edu.es>
2013-11-16 16:38:45 +0000

    Se cumple la espectativa para la clase matriz.

--- lib/lpp_t_04_matrix.rb
+++ lib/lpp_t_04_matrix.rb
@@ -1,5 +1,6 @@
 require "lpp_t_04_matrix/version"
 
 module LppT04Matrix
-  # Your code goes here...
+  class Matriz 
+  end
 end
--- spec/lpp_t_04_matrix_spec.rb
+++ spec/lpp_t_04_matrix_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-
+include  LppT04Matrix
 describe LppT04Matrix do
 	describe Matriz do
 	end
