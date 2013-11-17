e9dd95b01b5cc8edeb17710e34004f6530f32cf9
alu0100596113 <alu0100596113@ull.edu.es>
2013-11-17 17:51:01 +0000

    Se cumple la espectativa para restar Matriz Dispersa.

--- lib/lpp_t_04_matrix.rb
+++ lib/lpp_t_04_matrix.rb
@@ -69,5 +69,11 @@ module LppT04Matrix
 			elemento = @elemento.merge(other.elemento){|key, oldval, newval| oldval.merge(newval){|key2, oldval2, newval2|oldval2 + newval2}}
 			MatrizDispersa.new(@filas, other.columnas, elemento)
 		end
+
+		def -(other)
+			raise ArgumentError, "La longitud de las matrices no coincide." unless @filas == other.filas && @columnas == other.columnas
+			elemento = @elemento.merge(other.elemento){|key, oldval, newval| oldval.merge(newval){|key2, oldval2, newval2|oldval2 - newval2}}
+			MatrizDispersa.new(@filas, other.columnas, elemento)
+		end
 	end
 end
