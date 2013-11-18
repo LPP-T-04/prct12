436f928e8305108975952011021e3a9dad6f4464
EliezerCruzSuarez <alu0100611298@ull.edu.es>
2013-11-18 10:27:24 +0000

    Se incluye y se cumple la espectativa para restar una Matriz Densa con una Matriz Dispersa.

--- spec/lpp_t_04_matrix_spec.rb
+++ spec/lpp_t_04_matrix_spec.rb
@@ -143,6 +143,9 @@ describe LppT04Matrix do
 			it "Suma de dos matrices del orden mxn" do
 				@m1.+(@m2).to_s.should eq(@m4.to_s)
 			end
+			it "Resta de dos matrices del orden mxn" do
+				@m1.-(@m2).to_s.should eq(@m5.to_s)
+			end
 			it "Multiplicacion del orden 2x2 x 2x2" do
 				@m1.*(@m2).to_s.should eq(@m3.to_s)
 			end
