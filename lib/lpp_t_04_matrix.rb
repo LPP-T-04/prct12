4f07adb6ad026473c314bcff9900e684c8c2cf44
EliezerCruzSuarez <alu0100611298@ull.edu.es>
2013-11-18 10:58:18 +0000

    Se cumple la espectativa para restar una Matriz Dispersa con una Matriz Densa.

--- lib/lpp_t_04_matrix.rb
+++ lib/lpp_t_04_matrix.rb
@@ -132,8 +132,15 @@ module LppT04Matrix
 
 		def -(other)
 			raise ArgumentError, "La longitud de las matrices no coincide." unless @filas == other.filas && @columnas == other.columnas
-			elemento = @elemento.merge(other.elemento){|key, oldval, newval| oldval.merge(newval){|key2, oldval2, newval2|oldval2 - newval2}}
-			MatrizDispersa.new(@filas, other.columnas, elemento)
+			case other
+			when MatrizDensa
+				other.-(self)
+			when MatrizDispersa
+				elemento = @elemento.merge(other.elemento){|key, oldval, newval| oldval.merge(newval){|key2, oldval2, newval2|oldval2 - newval2}}
+				MatrizDispersa.new(@filas, other.columnas, elemento)
+			else
+				raise TypeError.new("Cannot coerce #{other.inspect} to a Matriz")
+			end
 		end
 
 		def *(other)
